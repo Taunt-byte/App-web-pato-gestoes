f22fa150915c641f5e0601d698cecf64aedf36af
Lucas Neves <lucasluan94@hotmail.com>
2021-11-03 17:49:52 -0300

    Update Geral
    Foram feitos o arquivo corona.php e houve mudanças nos arquivos html e css

---
+++ Pato-Gestao/corona.php
@@ -0,0 +1,24 @@
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://covid19-brazil-api.now.sh/api/report/v1/brazil/uf/sp",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET"
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
